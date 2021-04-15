6a7bbd6fa7fdc4e0a19134633f7b651ccf50db6d
Miguel Bastos <mig.silva@gmail.com>
2021-04-15 23:21:44 +0100

    add tex gregorio

--- install-essential.sh
+++ install-essential.sh
@@ -13,7 +13,8 @@ sudo apt-get install -y \
     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
     xz-utils tk-dev libffi-dev liblzma-dev \
     git nodejs npm python3-pip python3-neovim golang-go \
-    texlive-luatex texlive-fonts-extra latexmk
+    texlive-luatex texlive-fonts-extra latexmk \
+    libltdl7-dev flex bison fontforge python-pygments
 
 
 echo "1 - Essential linux packages installed"
@@ -23,16 +24,11 @@ echo "1 - Essential linux packages installed"
 if ! [ -x "$(command -v yarn)" ]; then
 	echo "2 - Yarn is installing"
   sudo npm install --global yarn
-else
-	echo "2 - Yarn already installed"
-
-  if ! [ -x "$(command -v prettier)" ]; then
+  elif ! [ -x "$(command -v prettier)" ]; then
     echo "2 - Yarn apps installing"
     yarn global add prettier eslint eslint_d typescript
   else
-    echo "2 - Prettier already installed"
-  fi
-
+    echo "2 - Yarn already installed"
 fi
 
 
@@ -45,7 +41,7 @@ if ! [ -d $HOME/build/neovim ]; then
     sudo make install
     sudo npm install --global neovim
 else
-	echo "3 - Neovim already installed"
+    echo "3 - Neovim already installed"
 fi
 
 # get dotfiles
@@ -54,14 +50,17 @@ if ! [ -d $HOME/.cfg ]; then
     git clone --bare https://github.com/ofrades/dotfiles ~/.cfg
     echo "5 - Moving dotfiles"
     git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout -f
+    echo "5 - Remove dotfiles"
+    sudo rm -r $HOME/dotfiles
+    git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 else
-	echo "5 - Dotfiles already present"
+    echo "5 - Dotfiles already present"
 fi
 
 # fish as default shell
-if ! [ -x "$(command -v fish)" ]; then
+if [ -x "$(command -v fish)" ] && ! []; then
     echo "6 - Setting fish as default shell"
-    chsh -s /usr/bin/fish
+    sudo chsh -s `which fish`
 fi
 
 # install starship
@@ -71,13 +70,13 @@ if ! [ -x "$(command -v starship)" ]; then
 fi
 
 # install pop shell
-if ! [ -d $HOME/build/shell ]; then
+if ! [ -d $HOME/build/shell ] ; then
     echo "8 - Pop shell installing"
     git clone https://github.com/pop-os/shell ~/build/shell
     cd ~/build/shell/
     make local-install
 else
-	echo "8 - Pop shell already installed"
+    echo "8 - Pop shell already installed"
 fi
 
 # install autojump
@@ -86,16 +85,16 @@ if ! [ -x "$(command -v jump)" ]; then
     wget https://github.com/gsamokovarov/jump/releases/download/v0.40.0/jump_0.40.0_amd64.deb
     sudo dpkg -i jump_0.40.0_amd64.deb
 else
-	echo "9 - Autojump already installed"
+    echo "9 - Autojump already installed"
 fi
 
 # install gregorio
-if ! [ -d $HOME/build/gregorio ]; then
+if ! [ -d $HOME/build/gregorio ] && ! [ -x "$(command -v gregorio)" ]; then
     echo "10 - Gregorio installing"
     git clone https://github.com/gregorio-project/gregorio ~/build/gregorio
     cd ~/build/gregorio
     ./build.sh
     sudo ./install.sh
 else
-	echo "10 - Gregorio already installed"
+    echo "10 - Gregorio already installed"
 fi
