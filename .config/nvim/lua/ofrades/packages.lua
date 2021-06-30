234f5dd6efdccb591e5b32d626574e50a5cb5de8
Miguel Bastos <mig.silva@gmail.com>
2021-06-30 16:44:10 +0100

    remove nlsp

--- .config/nvim/lua/ofrades/packages.lua
+++ .config/nvim/lua/ofrades/packages.lua
@@ -1,22 +1,19 @@
-local install_path = vim.fn.stdpath "data"
-	.. "/site/pack/packer/opt/packer.nvim"
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.api.nvim_command(
-		"!git clone https://github.com/wbthomason/packer.nvim " .. install_path
-	)
-	vim.api.nvim_command "packadd packer.nvim"
+	vim.api.nvim_command("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+	vim.api.nvim_command("packadd packer.nvim")
 end
 
 -- if using packer with opt
-vim.cmd [[ packadd packer.nvim ]]
+vim.cmd([[ packadd packer.nvim ]])
 
 -- sync when this file is changed
-vim.cmd [[ autocmd BufWritePost packages.lua PackerSync ]]
+vim.cmd([[ autocmd BufWritePost packages.lua PackerSync ]])
 
 return require("packer").startup(function()
-	use { "wbthomason/packer.nvim", opt = true }
-	use {
+	use({ "wbthomason/packer.nvim", opt = true })
+	use({
 		"mhinz/vim-startify",
 		opt = true,
 		event = "VimEnter",
@@ -43,17 +40,17 @@ return require("packer").startup(function()
 
 			vim.g.startify_custom_footer = { "Beauty will save the world! ><(((*>" }
 		end,
-	}
-	use {
+	})
+	use({
 		"tjdevries/express_line.nvim",
 		opt = true,
 		event = "VimEnter",
 		wants = "lsp-status.nvim",
 		config = function()
-			require "ofrades.statusline"
+			require("ofrades.statusline")
 		end,
-	}
-	use {
+	})
+	use({
 		"eddyekofo94/gruvbox-flat.nvim",
 		opt = true,
 		event = "VimEnter",
@@ -69,18 +66,18 @@ return require("packer").startup(function()
 			vim.g.gruvbox_transparent = true
 			vim.g.gruvbox_flat_style = "dark"
 			vim.g.gruvbox_italic_functions = true
-			vim.cmd "colorscheme gruvbox-flat"
+			vim.cmd("colorscheme gruvbox-flat")
 		end,
-	}
-	use {
+	})
+	use({
 		"kyazdani42/nvim-web-devicons",
 		opt = true,
 		event = "VimEnter",
 		config = function()
 			require("nvim-web-devicons").setup()
 		end,
-	}
-	use {
+	})
+	use({
 		"tamago324/lir.nvim",
 		opt = true,
 		event = "BufRead",
@@ -88,17 +85,17 @@ return require("packer").startup(function()
 			{
 				"tamago324/lir-git-status.nvim",
 				config = function()
-					require("lir.git_status").setup {
+					require("lir.git_status").setup({
 						show_ignored = false,
-					}
+					})
 				end,
 			},
 		},
 		config = function()
-			require "ofrades.explorer"
+			require("ofrades.explorer")
 		end,
-	}
-	use {
+	})
+	use({
 		"neovim/nvim-lspconfig",
 		event = "BufRead",
 		requires = {
@@ -110,20 +107,11 @@ return require("packer").startup(function()
 			{ "folke/lua-dev.nvim" },
 		},
 		config = function()
-			require "ofrades.lsphandlers"
-			require "ofrades.lspservers"
+			require("ofrades.lsphandlers")
+			require("ofrades.lspservers")
 		end,
-	}
-	use {
-		"tamago324/nlsp-settings.nvim",
-		opt = true,
-		event = "BufRead",
-		wants = "nvim-lspconfig",
-		config = function()
-			require("nlspsettings").setup()
-		end,
-	}
-	use {
+	})
+	use({
 		"andymass/vim-matchup",
 		opt = true,
 		event = "CursorMoved",
@@ -134,8 +122,8 @@ return require("packer").startup(function()
 				highlight = "Normal",
 			}
 		end,
-	}
-	use {
+	})
+	use({
 		"nvim-telescope/telescope.nvim",
 		event = "BufRead",
 		wants = "trouble.nvim",
@@ -148,18 +136,18 @@ return require("packer").startup(function()
 			{ "nvim-lua/plenary.nvim", module = "plenary" },
 		},
 		config = function()
-			require "ofrades.telescope"
+			require("ofrades.telescope")
 		end,
-	}
-	use {
+	})
+	use({
 		"lewis6991/gitsigns.nvim",
 		opt = true,
 		event = "BufRead",
 		config = function()
-			require "ofrades.gitsigns"
+			require("ofrades.gitsigns")
 		end,
-	}
-	use {
+	})
+	use({
 		"timuntersberger/neogit",
 		opt = true,
 		event = "BufRead",
@@ -175,10 +163,10 @@ return require("packer").startup(function()
 			},
 		},
 		config = function()
-			require "ofrades.neogit"
+			require("ofrades.neogit")
 		end,
-	}
-	use {
+	})
+	use({
 		"windwp/nvim-spectre",
 		event = "BufRead",
 		opt = true,
@@ -188,17 +176,17 @@ return require("packer").startup(function()
 			{ "nvim-lua/popup.nvim" },
 			{ "nvim-lua/plenary.nvim" },
 		},
-	}
-	use {
+	})
+	use({
 		"tpope/vim-commentary",
 		opt = true,
 		event = "BufRead",
-	}
-	use { "airblade/vim-rooter", opt = true, event = "BufRead" }
-	use { "mg979/vim-visual-multi", opt = true, event = "BufRead" }
-	use { "psliwka/vim-smoothie", opt = true, event = "BufRead" }
-	use { "rhysd/committia.vim", opt = true, event = "VimEnter" }
-	use {
+	})
+	use({ "airblade/vim-rooter", opt = true, event = "BufRead" })
+	use({ "mg979/vim-visual-multi", opt = true, event = "BufRead" })
+	use({ "psliwka/vim-smoothie", opt = true, event = "BufRead" })
+	use({ "rhysd/committia.vim", opt = true, event = "VimEnter" })
+	use({
 		"rrethy/vim-hexokinase",
 		opt = true,
 		run = "make hexokinase",
@@ -206,40 +194,40 @@ return require("packer").startup(function()
 		config = function()
 			vim.g.Hexokinase_highlighters = { "virtual" }
 		end,
-	}
-	use { "ggandor/lightspeed.nvim", opt = true, event = "BufRead" }
-	use {
+	})
+	use({ "ggandor/lightspeed.nvim", opt = true, event = "BufRead" })
+	use({
 		"folke/trouble.nvim",
 		opt = true,
 		event = "BufRead",
 		cmd = { "TroubleToggle", "Trouble" },
 		config = function()
-			require("trouble").setup { auto_open = false }
+			require("trouble").setup({ auto_open = false })
 		end,
-	}
-	use {
+	})
+	use({
 		"folke/which-key.nvim",
 		event = "VimEnter",
 		module = "which-key",
 		config = function()
-			require("which-key").setup {}
+			require("which-key").setup({})
 		end,
-	}
-	use {
+	})
+	use({
 		"folke/todo-comments.nvim",
 		opt = true,
 		event = "BufRead",
 		cmd = { "TodoTrouble", "TodoTelescope" },
 		config = function()
-			require("todo-comments").setup {}
+			require("todo-comments").setup({})
 		end,
-	}
-	use {
+	})
+	use({
 		"kristijanhusak/orgmode.nvim",
 		opt = true,
 		event = "BufRead",
 		config = function()
-			require("orgmode").setup {
+			require("orgmode").setup({
 				org_agenda_files = { "~/notes/*" },
 				org_default_notes_file = "~/notes/refile.org",
 				mappings = {
@@ -248,10 +236,10 @@ return require("packer").startup(function()
 						org_capture = "<leader>oc",
 					},
 				},
-			}
+			})
 		end,
-	}
-	use {
+	})
+	use({
 		"hrsh7th/nvim-compe",
 		event = "InsertEnter",
 		wants = "LuaSnip",
@@ -265,10 +253,10 @@ return require("packer").startup(function()
 				"L3MON4D3/LuaSnip",
 				event = "InsertCharPre",
 				config = function()
-					require("luasnip").config.set_config {
+					require("luasnip").config.set_config({
 						history = true,
 						updateevents = "TextChanged,TextChangedI",
-					}
+					})
 					require("luasnip/loaders/from_vscode").load()
 				end,
 				requires = {
@@ -277,22 +265,22 @@ return require("packer").startup(function()
 			},
 		},
 		config = function()
-			require "ofrades.completion"
+			require("ofrades.completion")
 		end,
-	}
-	use {
+	})
+	use({
 		"JoosepAlviste/nvim-ts-context-commentstring",
 		opt = true,
 		event = "BufRead",
-	}
-	use {
+	})
+	use({
 		"nvim-treesitter/nvim-treesitter",
 		opt = true,
 		event = "BufRead",
 		run = ":TSUpdate",
 		config = function()
-			require "ofrades.treesitter"
+			require("ofrades.treesitter")
 		end,
-	}
-	use { "tweekmonster/startuptime.vim", cmd = "StartupTime" }
+	})
+	use({ "tweekmonster/startuptime.vim", cmd = "StartupTime" })
 end)
